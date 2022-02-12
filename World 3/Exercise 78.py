954bdd36d11399532669256590ff628beb8ce433
Linek Reis <linekerreis12@hotmail.com>
2022-02-12 00:08:38 -0300

    Create Exercise 78.py

---
+++ World 3/Exercise 78.py
@@ -0,0 +1,6 @@
+numbers = []
+for c in range (0,5):
+    numbers.append(int(input("Type a value: ")))
+print(numbers)
+print(f"Highest value: {max(numbers)}")
+print(f"Lowest value: {min(numbers)}")
