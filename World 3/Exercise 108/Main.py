7fef5b17abc1a1b4890a782728c1ce7d592b1996
Linek Reis <linekerreis12@hotmail.com>
2022-02-27 13:00:26 -0800

    Update Main.py

--- World 3/Exercise 108/Main.py
+++ World 3/Exercise 108/Main.py
@@ -1,7 +1,7 @@
 import Coin
 
 r = float(input("Price of the product: "))
-print(f"USD {Coin.coin2(r)} Doubleded is: USD {Coin.double(r)}")
-print(f"Half of USD {Coin.coin2(r)} , is: USD {Coin.half(r)}")
+print(f"The double of {Coin.coin2(r)} {Coin.double(r)}")
+print(f"The half of {Coin.coin2(r)} , is: USD {Coin.half(r)}")
 print(f"{Coin.coin2(r)} Increased by 10% is: USD {Coin.percent(r,10)}")
 print(f"{Coin.coin2(r)} Decreased by 13% is: USD {Coin.reducing(r,13)}")
