317a6c27623c13844f7fbac70e4788a975ca38d7
Linek Reis <linekerreis12@hotmail.com>
2022-02-27 10:31:11 -0800

    Create Coin.py

---
+++ World 3/Exercise 107/Coin.py
@@ -0,0 +1,16 @@
+def half(number):
+    r = number / 2
+    return r
+
+def double(number):
+    r = number * 2 
+    return r
+
+def percent(number,percentage):
+    r = number + (number*(percentage/100))
+    return r
+
+def reducing(number,percentage):
+    r = number - (number*(percentage/100)) 
+    return r 
+
