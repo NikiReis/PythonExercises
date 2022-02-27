3e4b55e84d1fdb57f304bbfe5aa275039aa47e29
Linek Reis <linekerreis12@hotmail.com>
2022-02-27 14:04:14 -0800

    Create Coin.py

---
+++ World 3/Exercise 109/Coin.py
@@ -0,0 +1,27 @@
+def half(price=0):
+    r = price / 2
+    return r 
+
+
+def double(price=0):
+    r = price * 2 
+    return r 
+
+
+def percent(price=0, percentage=0):
+    r = price + (price*percentage/100)
+    return r 
+
+
+def reducing(price=0, percentage=0):
+    r = price - (price*percentage/100) 
+    return r 
+
+
+def coin2(price=0, valor='USD '):
+    return f'{valor}{price:.2f}'.replace('.', ',')
+  
+  """
+  The exercise 109.py didn't work in Python 3.10
+  but is basically the samething of the exercise 108.py
+  """
