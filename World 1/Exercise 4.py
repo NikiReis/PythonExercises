4400c84efa3d5118089065015f1c46603bda8d89
NikiReis <linekreis@hotmail.com>
2023-02-22 21:20:17 -0300

    Refactor: Dissecting a variable

--- World 1/Exercise 4.py
+++ World 1/Exercise 4.py
@@ -1,8 +1,12 @@
-value = input("Type a value:")
-print("---------------------")
-print("Class type: ",type(value))
-print("Is upper ?",value.isupper())
-print("Is lower ?",value.islower())
-print("Is alpha numeric ?",value.isalnum())
-print("Is alphabetic ?",value.isalpha())
-print("---------------------")
+def returns(value):
+    print("---------------------")
+    print("Class type: ", type(value))
+    print("Is upper ?", value.isupper())
+    print("Is lower ?", value.islower())
+    print("Is alpha numeric ?", value.isalnum())
+    print("Is alphabetic ?", value.isalpha())
+    print("---------------------")
+
+
+value = input("Type a value: ")
+returns(value)
