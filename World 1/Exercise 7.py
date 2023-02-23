e70b5847f711fb4ff06d5e51f462cb08a5045594
NikiReis <linekreis@hotmail.com>
2023-02-22 21:24:52 -0300

    Average grade of a student

--- World 1/Exercise 7.py
+++ World 1/Exercise 7.py
@@ -1,8 +1,19 @@
-print("-"*21)
-print("    School Average   ")
-print("-"*21)
-name = str(input("Name of the student:"))
-note1 = float(input("First note: "))
-note2 = float(input("Second note: "))
-average = (note1+note2)/2
-print("The average of the {} was : {} points".format(name,average))
+def student_avg(value1, value2):
+    return f'First grade: {value1}\nSecond grade" {value2}\nAverage student\'s grade is: {(value1+value2)/2}'
+
+
+try:
+    num1 = float(input('Type the first grade: '))
+    num2 = float(input('Type the second grade: '))
+    if num1 < 0 or num2 < 0:
+        print('Value out of index!! Please type a grande between 0 and 10')
+        exit()
+    else:
+        if num1 > 10:
+            num1 = 10
+        if num2 > 10:
+            num2 = 10
+except ValueError:
+    print(f'Please type only numeric values')
+else:
+    print(student_avg(num1, num2))
