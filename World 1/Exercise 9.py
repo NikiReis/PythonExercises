699d630a9c560e8f3a4351fcb2b03f86d45ab0c6
NikiReis <linekreis@hotmail.com>
2023-02-22 21:26:43 -0300

    Refactor: Times table

--- World 1/Exercise 9.py
+++ World 1/Exercise 9.py
@@ -1,16 +1,12 @@
-print("-"*9)
-print(" Times Table ")
-print("-"*9)
-number = int(input("Want to see the times table of wich number ? "))
-print("-"*11)
-print("{} x {} = {}".format(number,1,number*1))
-print("{} x {} = {}".format(number,2,number*2))
-print("{} x {} = {}".format(number,3,number*3))
-print("{} x {} = {}".format(number,4,number*4))
-print("{} x {} = {}".format(number,5,number*5))
-print("{} x {} = {}".format(number,6,number*6))
-print("{} x {} = {}".format(number,7,number*7))
-print("{} x {} = {}".format(number,8,number*8))
-print("{} x {} = {}".format(number,9,number*9))
-print("{} x {} = {}".format(number,10,number*10))
-print("-"*11)
+def multiplication_table(number):
+    print(f'The times table of {number} is: ')
+    for i in range(0, 10 + 1):
+        print(f'{i} x {number}: {i * number}')
+
+
+try:
+    num = int(input('Type a value: '))
+except ValueError:
+    print('Invalid Value! Please, type only numeric values')
+else:
+    multiplication_table(num)
