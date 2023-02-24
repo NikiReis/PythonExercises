480b9b9acfd034bda6ebcc9261f0eecc2f492431
NikiReis <linekreis@hotmail.com>
2023-02-23 16:48:01 -0800

    refactor: calculating discount percentage

--- World 1/Exercise 12.py
+++ World 1/Exercise 12.py
@@ -1,6 +1,6 @@
 def discount(value):
     if value == 0:
-        print('The product\'s price is zero, that means that the product is free')
+        print('The product price is zero, that means that the product is free')
         exit()
     else:
         return f'The product that coasted U$ {value}, now with 5% of discount is costing: U$ {value - (value*0.05):.2f}'
@@ -9,7 +9,7 @@ def discount(value):
 try:
     price = float(input('How much is the product ? '))
     if price < 0:
-        print('Doesn\'t exist negative prices')
+        print("Doesn't exist negative prices")
         exit()
 except ValueError:
     print('Please, type only numeric values!')
