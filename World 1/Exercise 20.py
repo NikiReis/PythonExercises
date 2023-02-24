986f596274d73d3372a72bea88cb0543ec1ef11a
NikiReis <linekreis@hotmail.com>
2023-02-23 17:02:02 -0800

    refactor: raffling of an order in the list

--- World 1/Exercise 20.py
+++ World 1/Exercise 20.py
@@ -1,12 +1,19 @@
-import random
-print("-"*21)
-print(" 'Prize Draw Part 2 '")
-print("-"*21)
-stdnt1 = str(input("First one: "))
-stdnt2 = str(input("Second : "))
-stdnt3 = str(input("Third : "))
-stdnt4 = str(input("Fourth : "))
-list = [stdnt1, stdnt2, stdnt3, stdnt4]
-random.shuffle(list)
-print("-"*21)
-print("The chosen order was:\n{}".format(list))
+def random_order(number):
+    from random import shuffle
+    students = []
+
+    for x in range(0, number):
+        names = str(input(f'Type the name of the {x + 1} student: '))
+        students.append(names)
+    shuffle(students)
+
+    return f'The chosen order was {students}'
+
+
+try:
+    print('Prize Draw')
+    quantity = int(input('How many students does the classroom has ? '))
+except ValueError:
+    print('Error! Type only numeric numbers!')
+else:
+    print(random_order(quantity))
