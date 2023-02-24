627b6588fb5dfd46786ce50af2ee501284f4fe7d
NikiReis <linekreis@hotmail.com>
2023-02-23 16:55:38 -0800

    refactor: opposite, adjacent side and hypotenuse of a triangle

--- World 1/Exercise 17.py
+++ World 1/Exercise 17.py
@@ -1,5 +1,14 @@
-from math import sqrt
-opposite = float(input("Type the opposite side of the triangle rectangle: "))
-adjside = float(input("Now type the adjacent side: "))
-hypotenuse = sqrt((opposite**2)+(adjside**2))
-print("The hypetonuse of this triangle is equivalent to: {:.2f}".format(hypotenuse))
+def triangle_hypotenuse(opposite, adjacent):
+    from math import hypot
+    hypotenuse = hypot(opposite, adjacent)
+    return f'{hypotenuse:.4f}'
+
+
+try:
+    number1 = float(input('Type the measure of the opposite triangle: '))
+    number2 = float(input('Type the measure of adjacent side of the triangle: '))
+except ValueError:
+    print('Please, type only numeric values: ')
+else:
+    print(triangle_hypotenuse(number1, number2))
+
