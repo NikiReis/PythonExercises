208ca56f20a87ae759d43babde9f2ca27f4556e7
NikiReis <linekreis@hotmail.com>
2023-02-23 17:03:03 -0800

    refactor: playing a sound from our computer

--- World 1/Exercise 21.py
+++ World 1/Exercise 21.py
@@ -1,11 +1,11 @@
-from playsound import playsound
-playsound('/path/to/a/sound/file/you/want/to//Blood on the Leaves.mp3')
+# import playsound
+# playsound('Path of the file')
 
-# I used the music Blood on the Leaves from Kanye West as demonstrative to make sure this exercise was going to work
 
 """
 if you want to try this code by your own in your PC 
 make sure to install the playsound package, version 1.2.2
 the newest versions for some reason it isn't in Python 3.10
-and if it stills not working, try downgrade the Python version to 3.7 or 3.8
+and if it stills not working, try downgrade the Python version to 3.7 or 3.8.
+and install the playsound package by 'pip install playground' command
 """
