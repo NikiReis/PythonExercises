e2ca40585116ab84cb479a485990dbde1b7cbf90
NikiReis <linekreis@hotmail.com>
2023-02-25 19:28:01 -0300

    refactor: Creating a speed tracker

--- World 1/Exercise 29.py
+++ World 1/Exercise 29.py
@@ -1,9 +1,17 @@
-velocity = float(input("What was the velocity that the car has passed on the radar ? "))
+def speed_check(speed):
+    if speed <= 80:
+        print('The car was under the permitted range, a fine wasn\'t applied ')
+    else:
+        print('Car\'s speed was higher than the legal range of speed, a fine has been applied')
+        return f'Fine\'s value U$ {(speed-80)*7.37:.2f}'
 
-if velocity <= 80:
-    print("The car was into the permited range, no one traffic ticket will be applied!")
+
+try:
+    velocity = float(input("What was the velocity that the car has passed on the radar ? "))
+    if velocity <= 0:
+        print('The speed can\'t be 0 or negative, otherwise you didn\'t pass through the speed tracker')
+        exit()
+except ValueError:
+    print('Please, type only numeric values, strings can\'t be accept !!')
 else:
-    print("The car was outide of the permited range, a traffic ticket has been applied!")
-    print("Max velocity: 80Km/h\nRegistered velocity: {}".format(velocity))
-    traffticket = (velocity-80)*7
-    print("Value of the traffic ticket: R$ {:.2f} !".format(traffticket))
+    print(speed_check(velocity))
