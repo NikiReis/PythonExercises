ba1b2afe22d81cf7713b0d1325f06b2b3f345876
NikiReis <linekreis@hotmail.com>
2023-02-24 16:53:30 -0800

    refactor: Analysing texts

--- World 1/Exercise 22.py
+++ World 1/Exercise 22.py
@@ -1,7 +1,15 @@
-print("-"*13)
-print("Name Analysis")
-print("-"*13)
-name = str(input("Write your name: ")).strip()
-print("-"*12)
-sort = name.split()
-print("You name only in upper: {}\nYour name only in lower: {}\nYour first name has: {} letters\nYour entire name has {}: letters".format(name.upper(), name.lower(), len(sort[0]), len(name)-name.count(" ")))
+def name_analysis(personame):
+    new_name = personame
+    personame = personame.split()
+    return f'You name only in upper: {new_name.upper()}\nYour name only in lower: {new_name.lower()}\nYour first name has: {len(personame[0])} letters\nYour entire name has {len(new_name)-new_name.count(" ")}: letters'
+
+
+try:
+    name = str(input("Type your complete name: "))
+    if name.isnumeric():
+        print('Please, type only names, numbers aren\'t going to be accept! ')
+        exit()
+except ValueError or AttributeError:
+    print('Somethin\'s wrong, please try again')
+else:
+    print(name_analysis(name))
