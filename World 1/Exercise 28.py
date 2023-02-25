aea2084b38148dbee16d6c4f6c9e54c34ec174e3
NikiReis <linekreis@hotmail.com>
2023-02-25 10:05:17 -0800

    refactor: Guess a number, mini-game

--- World 1/Exercise 28.py
+++ World 1/Exercise 28.py
@@ -1,9 +1,16 @@
-import random
+def guessing_number(guess):
+    import random
+    computer_number = random.randint(0, 10)
 
-number = random.randint(0,5)
-guess = int(input("Type the number that the computer thought: "))
+    if guess == computer_number:
+        print("Congrats, that's the right number!")
+    else:
+        print("Sorry! But that's not the number that the computer thought")
 
-if guess==number:
- print("Congrats, that's the right number!")
+
+try:
+    number = int(input('Type the number that the computer thought: '))
+except ValueError:
+    print('Please, type only numeric values')
 else:
-    print("Sorry!But that's not the number that the computer thought")
+    guessing_number(number)
