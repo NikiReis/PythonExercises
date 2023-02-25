8bcf1beb95f4ac5347e4ebfb0a522d120f39a5e9
NikiReis <linekreis@hotmail.com>
2023-02-25 19:31:25 -0300

    refactor: Leap year analysis

--- World 1/Exercise 32.py
+++ World 1/Exercise 32.py
@@ -1,9 +1,12 @@
 print("-"*12)
 print("Leap Year")
 print("-"*12)
-ano = int(input("Type a year: "))
-
-if ano%4==0 and (ano%100!=0) or ano%400==0:
-    print("The entered year is a leap year.")
-else: 
-    print("The entered year it's not a leap year.")
+try:
+    year = float(input("Type a year: "))
+except ValueError:
+    print('PLase, type only numeric values!')
+else:
+    if year % 4 == 0 and (year % 100 != 0) or year % 400 == 0:
+        print("The entered year is a leap year.")
+    else:
+        print("The entered year it's not a leap year.")
