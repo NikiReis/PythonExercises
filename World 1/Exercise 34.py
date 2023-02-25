9bd60c7cd5a67a1e2fb13d39539cda73236e4857
NikiReis <linekreis@hotmail.com>
2023-02-25 20:40:23 -0300

    refactor: employee salary raise v2.0

--- World 1/Exercise 34.py
+++ World 1/Exercise 34.py
@@ -1,11 +1,21 @@
 print("-"*15)
-print("Salary Adjustment v2")
+print("Salary Raise v2")
 print("-"*15)
 
-salary = float(input("What's the employe salary ? "))
+def salary_raise(salary):
+    if salary <= 2150:
+        new_salary = salary + (salary*0.15)
+    else:
+        new_salary = salary + (salary*0.10)
+    print(f'The salary adjusted is: R$ {new_salary:.2f}')
 
-if salary>1250:
-    newsalary = salary + (salary*0.15)
+
+try:
+    salary = float(input("What's the employee salary ? "))
+    if salary < 350:
+        print('The lower salary in the company is U$ 350,00, please try again!')
+        exit()
+except ValueError:
+    print('Somethin\'s wrong, please, try again!')
 else:
-    newsalary = salary + (salary*0.10)
-print("The salary adjusted is: R$ {}".format(newsalary))
+    salary_raise(salary)
