c4a57588c5a5072559696a77d073add2ec473d88
NikiReis <linekreis@hotmail.com>
2023-02-25 20:41:14 -0300

    refactor: Analysing a triangle

--- World 1/Exercise 35.py
+++ World 1/Exercise 35.py
@@ -2,11 +2,14 @@ print("-"*21)
 print("Forming a triangle")
 print("-"*21)
 
-side1 = float(input("Type the first side: "))
-side2 = float(input("Type the second side: "))
-side3 = float(input("Type the third side: "))
-
-if side1 < side2 + side3 and side2 < side1 + side3 and side3 < side1 + side2:
-    print("According to the sides entered\nis poissible to form a triangle!")
+try:
+    side1 = float(input("Type the first side: "))
+    side2 = float(input("Type the second side: "))
+    side3 = float(input("Type the third side: "))
+except ValueError:
+    print('You probably typed a text, please try again')
 else:
-    print("According to the sides entered\nit's not possible to form a triangle!")
+    if side1 < side2 + side3 and side2 < side1 + side3 and side3 < side1 + side2:
+        print("According to the sides entered\nis possible to form a triangle!")
+    else:
+        print("According to the sides entered\nit's not possible to form a triangle!")
