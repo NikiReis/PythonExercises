110195eaee45cc47061a61711f740b14d317eff0
NikiReis <linekreis@hotmail.com>
2023-02-25 19:30:35 -0300

    refactor: Trip value

--- World 1/Exercise 31.py
+++ World 1/Exercise 31.py
@@ -1,12 +1,18 @@
-print("-"*14)
-print("Km's Calculation")
-print("-"*14)
+def trip_value(kilometers):
 
-kms = int(input("How many kilometers did you drive ? "))
+    if kilometers == 0:
+        print('Apparently you rant 0 kms, so you don\'t have any bill to pay')
+    elif 0 < kilometers <= 200:
+        price = kms * 0.50
+        print("Price to be payed out equivalent\nto quantity of the kilometers that you have driven: U$ {}".format(price))
+    else:
+        price = kilometers * 0.45
+        print("Price to be payed out equivalent\nto quantity of the kilometers that you have driven:: U$ {}".format(price))
 
-if kms<=200:
-    price = kms*0.50
-    print("Price to be paied out equivalent\nto quantity of the kilometers that you have driven: R$ {}".format(price))
+
+try:
+    kms = float(input("How many kilometers did you drive ? "))
+except ValueError:
+    print('PLease we can\'t calculate the trip value if you\'re typing a text')
 else:
-    price = kms*0.45
-    print("Price to be paied out equivalent\nto quantity of the kilometers that you have driven:: R$ {}".format(price))
+    trip_value(kms)
