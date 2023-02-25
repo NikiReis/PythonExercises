a6fb475b2a74513846b9b27f23eb87cee23aeb24
NikiReis <linekreis@hotmail.com>
2023-02-25 10:02:20 -0800

    refactor: Verifying the first letters of a text

--- World 1/Exercise 24.py
+++ World 1/Exercise 24.py
@@ -1,6 +1,15 @@
-print("-"*17)
-print("City Analysis")
-print("-"*17)
-city = str(input("Write the name of you city: ")).strip()
-print("Your city has 'Saint' at start of the name ? ")
-print(city[:5].upper() == 'Saint')
+def city_analysis(city_name):
+    print("City Analysis")
+    print("Your city has 'Saint' at start of the name ? ")
+    print(city_name[:5].upper() == 'Saint')
+
+
+try:
+    city = str(input("Write the name of you city: ")).strip()
+    if city.isnumeric():
+        print('Please, don\'t type numbers, numbers can\'t be accept')
+        exit()
+except ValueError:
+    print('Somethin\'s wrong, please try again')
+else:
+    city_analysis(city)
